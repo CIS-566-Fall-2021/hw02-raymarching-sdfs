a23b031163eb50c0d45599a802728ee52e4b7271
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-09-30 13:23:47 -0400

    Got sphere rendering

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -111,23 +111,11 @@ vec3 getSceneColor(vec2 uv)
 
 void main()
 {
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fs_Pos/u_Dimensions.xy;
-    
-    // Make symmetric [-1, 1]
-    uv = uv * 2.0 - 1.0;
-
     // Time varying pixel color
-    vec3 col = getSceneColor(uv);
+    vec3 col = getSceneColor(fs_Pos);
 
     // Output to screen
     out_Col = vec4(col,1.0);
-}
 
-/*
-void main() 
-{
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
 }
-*/
 
