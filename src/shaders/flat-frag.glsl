50465d734c6d6cfb018006dd719f9d836aaaa555
Lanqing <seiseiko97@gmail.com>
2021-10-12 03:41:12 -0400

    second variation

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -143,7 +143,7 @@ vec4 axAng2Quat(vec3 ax, float ang)
 #define Z_AXIS vec3(0.0,0.0,1.0)
 #define X_AXIS vec3(1.0,0.0,0.0)
 #define Y_AXIS vec3(0.0,1.0,0.0)
-#define YZ_AXIS vec3(0.0,1.0,1.0)
+#define XZ_AXIS vec3(1.0,1.0,1.0)
 float gearRepeat(vec3 p){
 
     vec4 q = axAng2Quat(Z_AXIS,0.01*u_Time);
@@ -175,6 +175,38 @@ float firstset(vec3 p){
     //d = gearSDF(p);
     return d;
 }
+float gearRepeat2(vec3 p){
+
+    vec4 q = axAng2Quat(Z_AXIS,0.01*u_Time);
+    q =  multQuat(q,axAng2Quat(Y_AXIS,PI2/4.));
+    vec3 p1 = transformVecByQuat(p,q);
+
+    float g1 = gearSDF(p1);
+    q = axAng2Quat(Z_AXIS,-0.01*u_Time);
+    q =  multQuat(q,axAng2Quat(Y_AXIS,PI2/4.)); 
+    q = multQuat(q,axAng2Quat(Z_AXIS,-PI2/8.));
+    vec3 p2 = transformVecByQuat(0.92*p+vec3(0.5-0.34,-0.30,0.),q);
+    float g2 = gearSDF(p2);
+    vec3 p3 = p+vec3(0.5,-0.5,0.);
+    q = axAng2Quat(Z_AXIS,-0.01*u_Time);
+    q =  multQuat(q,axAng2Quat(X_AXIS,PI2/4.)); 
+    p3 = transformVecByQuat(p3,q);
+    float g3 = gearSDF(p3);
+
+    return min(min(g1,g2),g3);
+}
+
+float secondset(vec3 p){
+    p = p + vec3(0.,0.5,0.);
+    float d = gearRepeat2(p);
+    vec3 p0 = p*vec3(1.,1.,1.);
+    vec3 p1 = p*vec3(-1.,1.,1.);
+    d = min(d,gearRepeat2(p1.xzy+vec3(0.,0.5,-0.5)));
+    p0 = p0 *(-1.,1.,-1.);
+    p0 = p0.zyx;
+    d = min(d,gearRepeat2(p0.xyz+vec3(0.,1.0,0.)));
+    return d;
+}
 float sceneSDF(vec3 p){
     vec3 mod_p;
     mod_p = fract(p)-0.5;
@@ -182,8 +214,8 @@ float sceneSDF(vec3 p){
     vec3 id = floor(mod(p,2.))*2.-1.;
     float checker = id.x*id.y*id.z;
     //mod_p = p-0.5;
-    float d = firstset(checker*mod_p);
-    
+    float d = secondset(checker*mod_p);
+    //d = secondset(p);
     return d;
 }
 
