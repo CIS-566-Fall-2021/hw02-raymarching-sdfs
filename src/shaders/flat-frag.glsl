646f4fb6723b95095a2a3663ea8595fcc41d001a
dluisnothere <43430369+dluisnothere@users.noreply.github.com>
2021-10-13 21:44:37 -0400

    light fix

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -13,6 +13,7 @@ int MAX_RAY_STEPS = 128;
 float FOV = 45.0;
 float EPSILON = 1e-3;
 vec3 LIGHT_DIR = vec3(-1.0, 5.0, -5.0);
+vec3 LIGHT_DIR2 = vec3(2.0, 5.0, -5.0);
 const float HALF_PI = 3.14159 * 0.5;
 
 // OBJ IDS
@@ -21,8 +22,12 @@ int SOUPFACE = 1;
 int SOUPEYES = 2;
 int BOWL = 3;
 int SOUPMOUTH = 4;
+int CARROT = 5;
+int PEA = 6;
 
 // COLORS
+vec3 LIGHT2COLOR = vec3(0.5, 0.0, 0.0);
+
 vec3 AMBIENTCOLOR = vec3(0.3);
 vec3 SOUPCOLOR = vec3(255.0, 246.0, 196.0) / 255.0;
 vec3 TABLECOLOR = vec3(150.0, 13.0, 0.0) / 255.0;
@@ -31,13 +36,16 @@ vec3 SOUPEYECOLOR = vec3(255.0, 252.0, 245.0) / 255.0;
 vec3 SOUPIRISCOLOR = vec3(235.0, 149.0, 52.0) / 255.0;
 vec3 SOUPMOUTHCOLOR = vec3(230.0, 203.0, 165.0) / 255.0;
 vec3 SOUPTEXTURECOLOR = vec3(247.0, 216.0, 74.0) / 255.0;
+vec3 CARROTCOLOR = vec3(214.0, 94.0, 2.0) / 255.0;
+vec3 PEACOLOR = vec3(0.0, 148.0, 7.0) / 255.0;
 
 // SDF CONSTANTS
 float S1RADIUS = 7.0;
 float S2RADIUS = 6.6;
 float SOUPRADIUS = 9.0;
 vec3 TABLEDIMS = vec3(20.0, 3.0, 20.0);
-vec3 BOUNDINGBOXDIMS = vec3(25.0, 50.0, 25.0);
+vec3 BOUNDINGBOXDIMS = vec3(25.0, 20.0, 25.0);
+vec3 CARROTDIMS = vec3(0.8);
 float IRISRADIUS = 0.52;
 vec2 IRISOFFSET = vec2(0.5, 1.0); // vec2(x, z) not vec2(x, y)
 
@@ -103,11 +111,76 @@ float fbmNoise(vec3 v)
     return total;
 }
 
+float getAnimation() {
+    return sin(float(u_Time) * 0.01) * 2.0;
+}
+
 vec3 getSoupFaceColor(vec3 pos) {
   //return clamp(fbmNoise(pos), 0.1, 0.3) * SOUPTEXTURECOLOR + SOUPCOLOR;
-  float noiseFac = clamp(fbmNoise(pos), 0.0, 1.0);
+  vec3 noiseInput = pos + getAnimation();
+  float noiseFac = clamp(fbmNoise(noiseInput), 0.0, 1.0);
   return mix(SOUPCOLOR, SOUPTEXTURECOLOR, noiseFac);
 }
+
+float random3D2(vec3 p) {
+    return sin(length(vec3(fract(dot(p, vec3(6.1, 2.8, 6.2))), 
+                            fract(dot(p, vec3(2.5, 6.3, 6.4))),
+                            fract(dot(p, vec3(6.4, 6.2, 2.5))))) * 459.0906);
+}
+
+float interpolateNoise3D2(float x, float y, float z)
+{
+    int intX = int(floor(x));
+    float fractX = fract(x);
+    int intY = int(floor(y));
+    float fractY = fract(y);
+    int intZ = int(floor(z));
+    float fractZ = fract(z);
+
+    float v1 = random3D2(vec3(intX, intY, intZ));
+    float v2 = random3D2(vec3(intX + 1, intY, intZ));
+    float v3 = random3D2(vec3(intX, intY + 1, intZ));
+    float v4 = random3D2(vec3(intX + 1, intY + 1, intZ));
+
+    float v5 = random3D2(vec3(intX, intY, intZ + 1));
+    float v6 = random3D2(vec3(intX + 1, intY, intZ + 1));
+    float v7 = random3D2(vec3(intX, intY + 1, intZ + 1));
+    float v8 = random3D2(vec3(intX + 1, intY + 1, intZ + 1));
+
+
+    float i1 = mix(v1, v2, fractX);
+    float i2 = mix(v3, v4, fractX);
+
+    //mix between i1 and i2
+    float i3 = mix(i1, i2, fractY);
+
+    float i4 = mix(v5, v6, fractX);
+    float i5 = mix(v7, v8, fractX);
+
+    //mix between i3 and i4
+    float i6 = mix(i4, i5, fractY);
+
+    //mix between i3 and i6
+    float i7 = mix(i3, i6, fractZ);
+
+    return i7;
+}
+
+float fbmNoise2(vec3 v) {
+    float total = 0.0;
+    float persistence = 0.5;
+    float frequency = 5.0;
+    float amplitude = 5.0;
+    int octaves = 5;
+
+    for (int i = 1; i <= octaves; i++) {
+        total += amplitude * interpolateNoise3D2(frequency * v.x, frequency * v.y, frequency * v.z);
+        frequency *= 3.6;
+        amplitude *= persistence;
+    }
+    return total;
+}
+
 // SCENE STRUCTS
 struct Ray 
 {
@@ -187,16 +260,20 @@ float sdCappedCylinder( vec3 p, float h, float r )
   return min(max(d.x,d.y),0.0) + length(max(d,0.0));
 }
 
-float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+float sdVerticalCapsule( vec3 p, float h, float r )
 {
-  vec3 pa = p - a, ba = b - a;
-  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
-  return length( pa - ba*h ) - r;
+  p.y -= clamp( p.y, 0.0, h );
+  return length( p ) - r;
 }
 
 float rounding( in float d, in float h )
 {
     return d - h;
+}
+
+float planeSDF(vec3 queryPos, float height)
+{
+    return height - queryPos.y;
 }
 
 // CSG FUNCTIONS
@@ -320,7 +397,6 @@ float faceSDF(vec3 queryPos, inout int objHit) {
 
     float smoothlip2 = opSmoothUnion(soupBase, smoothlip, 0.2);
     
-    //chicken soup bits
     float t = soupBase;
     objHit = SOUPFACE;
     t = min(t, soupLEyelid);
@@ -349,22 +425,47 @@ float sceneSDF(vec3 queryPos, inout int objHit)
 
   if (boundingBoxDist < EPSILON) {
     float table = sdBox(queryPos + vec3(0.0, 8.0, 0.0), TABLEDIMS);
+    
     float sphere1 = sdfSphere(queryPos, vec3(0.0), S1RADIUS); 
     float innerSphere1 = sdfSphere(queryPos, vec3(0.0), S2RADIUS);
+    float sphereSlicePlane = planeSDF(queryPos + vec3(0.0, 5.0, 0.0), 5.0);
+
     float bowl = opSubtraction(innerSphere1, sphere1);
+    bowl = opSubtraction(sphereSlicePlane, bowl);
 
     //When soup is not being crazy -- Credit to Sam for helping me figure this out
     float normalSoup = sdfSphere(queryPos, vec3(0.0), S1RADIUS);
     normalSoup = max(normalSoup, queryPos.y + 0.5);
 
     //mix flat soup with OWO soup
-    float face = mix(faceSDF(queryPos, objHit), normalSoup, clamp(cos(u_Time * 0.1), 0.0, 1.0));
-    
+    float face = mix(faceSDF(queryPos, objHit), normalSoup, clamp(cos(u_Time * 0.05), 0.0, 1.0));
+
+    //soup bits
+    //vec3 inputPosition = vec3(queryPos.x, sin(u_Time * 0.001), queryPos.z);
+    float carrot1 = sdBox(vec3(rotateZ(40.0 * 3.14159 / 180.0) * rotateX(30.0 * 3.14159 / 180.0) * (vec4(queryPos, 1.0) + vec4(-1.0, 0.5, 5.0, 1.0))), CARROTDIMS);
+    float carrot2 = sdBox(vec3(rotateX(50.0 * 3.14159 / 180.0) * (vec4(queryPos, 1.0) + vec4(3.0, 0.5, 4.0, 1.0))), CARROTDIMS * 0.7);
+    float carrot3 = sdBox(vec3(rotateY(70.0 * 3.14159 / 180.0) * rotateZ(30.0 * 3.14159 / 180.0) * (vec4(queryPos, 1.0) + vec4(-2.0, 0.5, -4.0, 1.0))), CARROTDIMS * 0.7);
+    float carrotSmooth = opSmoothUnion(carrot1, face, 0.3);
+    carrotSmooth = opSmoothUnion(carrot2, carrotSmooth, 0.3);
+    carrotSmooth = opSmoothUnion(carrot3, carrotSmooth, 0.3);
+
+    float pea1 = sdfSphere(vec3(queryPos + vec3(4.0, 0.5, 0.0)), vec3(0.0), 0.5);
+    float pea2 = sdfSphere(vec3(queryPos + vec3(-5.0, 0.5, 3.0)), vec3(0.0), 0.5);
+    float pea3 = sdfSphere(vec3(queryPos + vec3(-3.0, 0.5, -3.0)), vec3(0.0), 0.5);
+    float pea4 = sdfSphere(vec3(queryPos + vec3(3.0, 0.5, 3.0)), vec3(0.0), 0.5);
+
     /* the max function slices sphere in half
     * courtesy of IQ's demo here: https://www.shadertoy.com/view/MlcBDj
     */
-    float t = max(min(table, bowl), queryPos.y);
+    float t = min(table, bowl);
     t = min(t, face);
+    t = min(t, carrot1);
+    t = min(t, carrot2);
+    t = min(t, carrot3);
+    t = min(t, pea1);
+    t = min(t, pea2);
+    t = min(t, pea3);
+    t = min(t, pea4);
 
     if (t == table) {
       objHit = TABLE;
@@ -372,6 +473,14 @@ float sceneSDF(vec3 queryPos, inout int objHit)
     if (t == bowl) {
       objHit = BOWL;
     }
+    if (t == carrot1 || t == carrot2 || t == carrot3) {
+      objHit = CARROT;
+    }
+    if (t == pea1 || t == pea2 || t == pea3 || t == pea4) {
+      objHit = PEA;
+    }
+
+    t = min(t, carrotSmooth);
 
     return t;
   }
@@ -390,6 +499,18 @@ float shadow(vec3 rayOrigin, inout int objHit, vec3 rayDirection, float min_t, f
   return 1.0;
 }
 
+float softShadow(vec3 rayOrigin, inout int objHit, vec3 rayDirection, float min_t, float max_t, float k) {
+  float res = 1.0;
+  for (float t = min_t; t < max_t; ) {
+    float h = sceneSDF(rayOrigin + rayDirection * t, objHit);
+    if (h < EPSILON) {
+      return 0.0;
+    }
+    res = min(res, k * h / t);
+    t += h;
+  }
+  return 1.0;
+}
 
 Ray getRay(vec2 ndcPos) {
   Ray r;
@@ -463,14 +584,23 @@ vec3 getSceneColor(vec2 uv) {
 
       // CALCULATE ALL COLOR
       int shadowHitObject;
-      float shadowFactor = shadow(intersection.position, shadowHitObject, normalize(LIGHT_DIR), 0.1, 7.0);
+      //float shadowFactor = shadow(intersection.position, shadowHitObject, normalize(LIGHT_DIR), 0.1, 7.0);
+      float shadowFactor = softShadow(intersection.position, shadowHitObject, normalize(LIGHT_DIR), 0.1, 7.0, 0.5);
       vec3 materialColor = AMBIENTCOLOR;
       if (intersection.objectHit == SOUPFACE) {
         materialColor = getSoupFaceColor(intersection.position);
+        // CALCULATE BLINN PHONG LIGHTING FOR SOUP FACE
+
+        vec3 V = normalize(vec3(u_Eye) - vec3(intersection.position));
+        vec3 L = normalize(LIGHT_DIR - vec3(intersection.position));
+        vec4 avg_h = vec4((V + L) / 2.0, 0.0);
+        float specIntensity = max(pow(clamp(dot(avg_h, vec4(intersection.normal, 0.0)), 0.0, 1.0), 10.0), 0.0);
+        
+        materialColor = getSoupFaceColor(intersection.position) * (lambertIntensity + specIntensity);
       } else if (intersection.objectHit == TABLE) {
-        materialColor = TABLECOLOR;
+        materialColor = TABLECOLOR * lambertIntensity;
       } else if (intersection.objectHit == BOWL) {
-        materialColor = BOWLCOLOR;
+        materialColor = BOWLCOLOR * lambertIntensity;
       } else if (intersection.objectHit == SOUPEYES) {
 
         // POLKA DOT PATTERN 
@@ -479,14 +609,18 @@ vec3 getSceneColor(vec2 uv) {
         vec2 nearest = 2.0 * fract(frequency * (vec2(intersection.position.x, intersection.position.z) - IRISOFFSET)) - 1.0;
         float dist = length(nearest);
         float radius = IRISRADIUS;
-        materialColor = mix(SOUPIRISCOLOR, SOUPEYECOLOR, step(radius, dist));
+        materialColor = mix(SOUPIRISCOLOR, SOUPEYECOLOR, step(radius, dist)) * lambertIntensity;
       } else if (intersection.objectHit == SOUPMOUTH) {
-        materialColor = mix(SOUPMOUTHCOLOR, SOUPCOLOR, intersection.position.y);
+        materialColor = mix(SOUPMOUTHCOLOR, SOUPCOLOR, intersection.position.y) * lambertIntensity;
+      } else if (intersection.objectHit == CARROT) {
+        materialColor = CARROTCOLOR * lambertIntensity;
+      } else if (intersection.objectHit == PEA) {
+        materialColor = PEACOLOR * lambertIntensity;
       }
       vec3 finalcolor = materialColor * shadowFactor;
-      return finalcolor * lambertIntensity;
+      return finalcolor;
     }
-  return vec3(0.3);
+  return vec3(clamp(fbmNoise2(vec3(uv, 1.0) + getAnimation()), 0.0, 0.06));
 }
 
 //MAIN
